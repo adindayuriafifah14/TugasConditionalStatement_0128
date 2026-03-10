15f8ccc229453ea4f4dac95c2c01f1ffd634e42d
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-03-10 21:39:46 +0800

    Memasukkan prosedur input data

--- Praktikum4.cpp
+++ Praktikum4.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 #include <string>
 using namespace std;
+ //Prosedur untuk memasukkan data berat dan tinggi
+ void inputData(float & berat, float & tinggi){
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (m): ";
+    cin >> tinggi;
+ }
